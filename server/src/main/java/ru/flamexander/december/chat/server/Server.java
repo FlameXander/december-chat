fed4b02465a8b89bb544a1acad9dfe75a316159d
Алексей Гильдебрант <mashinis@gmail.com>
2024-01-19 19:06:48 +0800

    вернул все изменения в ClientHandler2

--- server/src/main/java/ru/flamexander/december/chat/server/Server.java
+++ server/src/main/java/ru/flamexander/december/chat/server/Server.java
@@ -27,7 +27,8 @@ public class Server {
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.printf("Сервер запущен на порту %d. Ожидание подключения клиентов\n", port);
-            userService = new InJdbcUserService();
+            // Используем БД Postgres
+            userService = new InMemoryUserService();
             System.out.println("Запущен сервис для работы с пользователями");
             while (true) {
                 Socket socket = serverSocket.accept();
