5106b717d38ba362cb15826f403b3cca2be7efef
Stbran <mistral-ya@yandex.ru>
2024-01-31 00:16:51 +0300

    авторизация + регистрация и хранение юзеров

--- client/src/main/java/ru/flamexander/december/chat/client/ClientApplication.java
+++ client/src/main/java/ru/flamexander/december/chat/client/ClientApplication.java
@@ -4,10 +4,11 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class ClientApplication {
+    static String username;
+
     public static void main(String[] args) {
         try (
                 Socket socket = new Socket("localhost", 8189);
@@ -18,6 +19,16 @@ public class ClientApplication {
             Scanner scanner = new Scanner(System.in);
             new Thread(() -> {
                 try {
+                    while (true) {
+                        String message = in.readUTF();
+                        if (message.startsWith("/")) {
+                            if (message.startsWith("/authok ")) {
+                                username = message.split(" ")[1];
+                                break;
+                            }
+                        }
+                        System.out.println(message);
+                    }
                     while (true) {
                         String message = in.readUTF();
                         System.out.println(message);
---
+++ server/src/main/java/ru/flamexander/december/chat/server/UserService.java
@@ -0,0 +1,8 @@
+package ru.flamexander.december.chat.server;
+
+public interface UserService {
+    String getUsernameByLoginAndPassword(String login, String password);
+    void createNewUser(String login, String password, String username);
+    boolean isLoginAlreadyExist(String login);
+    boolean isUsernameAlreadyExist(String username);
+}
